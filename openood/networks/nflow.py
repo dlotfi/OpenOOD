4bf6f26c07a7c7bc33fc9c9ab554d4b5c55c0576
dlotfi <lotfi.developer@live.com>
2024-07-24 11:29:08 +0800

    Some trivial refactoring

--- openood/networks/nflow.py
+++ openood/networks/nflow.py
@@ -3,14 +3,15 @@ import torch
 
 
 class ClampedMLP(nf.nets.MLP):
-    def __init__(self, min=None, max=None, **kwargs):
+    def __init__(self, clamp_value=None, **kwargs):
         super().__init__(**kwargs)
-        self.min = min
-        self.max = max
+        self.clamp_value = clamp_value
 
     def forward(self, x):
         x = super().forward(x)
-        x = torch.clamp(x, min=self.min, max=self.max)
+        if self.clamp_value is None:
+            return x
+        x = torch.clamp(x, min=-self.clamp_value, max=self.clamp_value)
         return x
 
 
@@ -24,12 +25,10 @@ def get_normalizing_flow(network_config):
     b = torch.Tensor([1 if i % 2 == 0 else 0 for i in range(latent_size)])
     flows = []
     for i in range(n_flows):
-        s = ClampedMLP(max=clamp_value,
-                       min=-clamp_value,
+        s = ClampedMLP(clamp_value=clamp_value,
                        layers=[latent_size, hidden_size, latent_size],
                        init_zeros=True)
-        t = ClampedMLP(max=clamp_value,
-                       min=-clamp_value,
+        t = ClampedMLP(clamp_value=clamp_value,
                        layers=[latent_size, hidden_size, latent_size],
                        init_zeros=True)
         if i % 2 == 0:
--- scripts/osr/nflow/organamnist_224_agg_train_nflow.sh
+++ scripts/osr/nflow/organamnist_224_agg_train_nflow.sh
@@ -29,4 +29,4 @@ python main.py \
     --network.backbone.encoder.checkpoint "./results/organamnist_resnet18_224x224/s${SEED}/resnet18_224_1.pth" \
     --network.backbone.encoder.checkpoint_key "net" \
     --seed ${SEED}
-    # --optimizer.weight_decay 0.0001
+    # --optimizer.weight_decay 0.0001  # in case of non-finite gradient norm error
