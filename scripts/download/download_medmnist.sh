99ad4cb8ca43a4d13eb3068febad849ccfb0fd55
dlotfi <lotfi.developer@live.com>
2025-01-19 12:41:26 +0800

    Fix download_medmnist scripts; Remove an optional package from requirements.txt

--- scripts/download/download_medmnist.sh
+++ scripts/download/download_medmnist.sh
@@ -3,14 +3,7 @@
 python ./scripts/download/download_medmnist.py \
 	--datasets 'organamnist' 'organcmnist' 'organsmnist' 'pneumoniamnist' \
              'chestmnist' 'dermamnist' 'retinamnist' 'breastmnist' \
-             'bloodmnist' 'tissuemnist' 'pathmnist'  \
-	--save_dir './data/medmnist_plus' \
-	--imglist_dir './data/benchmark_imglist/medmnist_plus' \
-	--size 224
-
-#python ./scripts/download/download_medmnist.py \
-#	--datasets 'organmnist3d' 'nodulemnist3d' 'fracturemnist3d' \
-#	           'adrenalmnist3d' 'vesselmnist3d' 'synapsemnist3d' \
-#	--save_dir './data/medmnist3d' \
-#	--imglist_dir './data/benchmark_imglist/medmnist3d' \
-#	--size 64
+             'bloodmnist' 'pathmnist'  \
+	--save_dir './data/medmnist' \
+	--imglist_dir './data/benchmark_imglist/medmnist' \
+	--size 28
--- scripts/download/download_medmnist_plus.sh
+++ scripts/download/download_medmnist_plus.sh
@@ -3,7 +3,7 @@
 python ./scripts/download/download_medmnist.py \
 	--datasets 'organamnist' 'organcmnist' 'organsmnist' 'pneumoniamnist' \
              'chestmnist' 'dermamnist' 'retinamnist' 'breastmnist' \
-             'bloodmnist' 'tissuemnist' 'pathmnist'  \
+             'bloodmnist' 'pathmnist'  \
 	--save_dir './data/medmnist_plus' \
 	--imglist_dir './data/benchmark_imglist/medmnist_plus' \
 	--size 224
